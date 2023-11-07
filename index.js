0435d6e05ebf67cddfd188e37998de0bd06946fc
Lynjai Jimenez <139661024+IntermediaDesigns@users.noreply.github.com>
2023-11-07 16:14:32 -0500

    2

--- index.js
+++ index.js
@@ -1,52 +1,44 @@
-const input1 = prompt(
+const response = prompt(
   'What program would you like to run? (length, yell, binary, or numSum?)'
 )
 
-if (input1 === 'length') {
-  const userInput = prompt(
+if (response === 'length') {
+  const enterAnswer = prompt(
     'Enter your word to calculate the number of characters:'
   )
 
   let count = 0
   let index = 0
-  
-  while (userInput[index]) {
+
+  while (enterAnswer[index]) {
     count = count + 1
     index = index + 1
   }
 
-alert(`${userInput} has ${count} characters.`)
-} 
-
-else if (input1 === 'yell') {
+  alert(`${enterAnswer} has ${count} characters.`)
+} else if (response === 'yell') {
   const userInput = prompt('Enter your word to yell:')
   let output = ''
-  for (let i = 0; i < userInput.length; i++) {
-    output = output + userInput[i].toUpperCase()
+  for (let i = 0; i < enterAnswer.length; i++) {
+    output = output + enterAnswer[i].toUpperCase()
   }
   alert(output)
-} 
-
-else if (input1 === 'binary') {
-  const userInput = prompt('Enter string to filter for binary:')
+} else if (response === 'binary') {
+  const enterAnswer = prompt('Enter string to filter for binary:')
   let output = ''
-  for (let i = 0; i < userInput.length; i++) {
-    if (userInput[i] == 0 || userInput[i] == 1) output += userInput[i]
+  for (let i = 0; i < enterAnswer.length; i++) {
+    if (userInput[i] == 0 || enterAnswer[i] == 1) output += enterAnswer[i]
   }
   alert(output)
-} 
-
-else if (input1 === 'numSum') {
-  const userInput = prompt(
+} else if (response === 'numSum') {
+  const enterAnswer = prompt(
     'Enter number to calculate the sum of all the numbers it contains:'
   )
   let num = 0
-  for (let i = 0; i <= userInput; i++) {
+  for (let i = 0; i <= enterAnswer; i++) {
     num = num + i
   }
   alert(num)
-} 
-
-else {
+} else {
   alert("I don't recognize that command.")
 }
